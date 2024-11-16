a342a52acd3f7c363aff5d58b3c495f4bed8f398
Artifacttx <patea933@gmail.com>
2024-11-16 19:28:11 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,21 +1,15 @@
 --why are you here?
 --bro skided
-if not game:IsLoaded() then
-    repeat
-        task.wait()
-    until game:IsLoaded()
-end
-
 local log = loadstring(game:HttpGet("https://raw.githubusercontent.com/Artifacttx/YumeHub/refs/heads/main/Script/Unknow.lua"))()
 
 _G.GameList = {
 	[537413528] = "https://raw.githubusercontent.com/Artifacttx/YumeHub/main/Script/Build%20a%20boat.lua",
 	[3101667897] = "https://raw.githubusercontent.com/Artifacttx/YumeHub/main/Script/Legends%20of%20Speed.lua",
-	[3232996272] = "https://raw.githubusercontent.com/Artifacttx/YumeHub/main/Script/Legends%20of%20Speed.lua",
+	[3232996272] = "https://raw.githubusercontent.com/Artifacttx/YumeHub/main/Script/Legends%20of%20Speed.lua"
 }
 _G.CheckGame = _G.GameList[game.PlaceId]
 if _G.GameList[game.PlaceId] then
 loadstring(game:HttpGet(_G.CheckGame))()
 else
-game.Players.LocalPlayer:Kick("Not Game Support")
+game.Players.LocalPlayer:Kick("Game Not Support")
 end
